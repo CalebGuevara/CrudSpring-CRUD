6de7cbdf7d1f97b45c8b7fe472444d0d19b90d5b
CalebGuevara <94706380+CalebGuevara@users.noreply.github.com>
2023-03-31 21:43:06 -0500

    Create import.sql

---
+++ import.sql
@@ -0,0 +1,3 @@
+insert into personas(nombre, apellido, correo) values('Caleb', 'Guevara', 'caleb.guevara@hotmail.com');
+insert into personas(nombre, apellido, correo) values('John', 'Doe', 'john.doe@hotmail.com');
+insert into personas(nombre, apellido, correo) values('Jane', 'Doe', 'jane.doe@hotmail.com');
